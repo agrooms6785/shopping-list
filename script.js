ee014f07dcb99112d162a64234fba5bb87549e53
Amanda Grooms <amandafgrooms@gmail.com>
2019-10-28 13:32:35 -0500

    some js updates

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //this is my local js file
+$(function() {
+  $('#js-shopping-list-form').submit(event => {
+    // stop the default submission behavior
+    event.preventDefault();
+    const newItem = $('.js-shopping-list-entry').val();
+  //allow user the ability to clear inputs
+  $('.shopping-list-entry').val('');
+  //allow user to add new item to list
+  $('.shopping-list').append(
+    `<li>
+      <span class="shopping-item">${newItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li`>
+  });
+
+  $('.shopping-item-controls').on('click', function() {
+    $('.shopping-item-toggle').toggleClass('.shopping-item__checked' > '.shopping-item-toggle')
+  } )
+}
