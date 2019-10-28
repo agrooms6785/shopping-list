54dca3892114b8588755b0301eacf30f2d919bf3
Amanda Grooms <amandafgrooms@gmail.com>
2019-10-28 14:24:09 -0500

    more js updates

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //this is my local js file
 $(function() {
-  $('#js-shopping-list-form').submit(event => {
+  $('#js-shopping-list-form').submit(function(event) {
     // stop the default submission behavior
     event.preventDefault();
-    const newItem = $('.js-shopping-list-entry').val();
+    const newItem = $('.shopping-list-entry').val();
   //allow user the ability to clear inputs
   $('.shopping-list-entry').val('');
   //allow user to add new item to list
@@ -18,16 +18,15 @@ $(function() {
           <span class="button-label">delete</span>
         </button>
       </div>
-    </li`>
-  });
+    </li>`);
+});
 // to strike through checked off items
   $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
     $(this).closest('li').toggleClass('shopping-item__checked');
-  } );
+  });
 
 // to delete items
   $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
     $(this).closest('li').find('.shopping-item').remove();
-  } );
-}
+  });
 }
